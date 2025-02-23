1fe1e320f6ce1f5c425355e785b12642dc5c13fc
Daniel <dadumbdan@gmail.com>
2025-02-23 17:50:37 +0500

    Fix API URL for deployment

--- phonebook-backend/server.js
+++ phonebook-backend/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-const cors = require('cors');
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :response-time ms - :body'));
+
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const personsRoutes = require('./api/persons');
 const infoRoutes = require('./api/info');
@@ -16,13 +18,13 @@ const infoRoutes = require('./api/info');
 app.use('/api/persons', personsRoutes);
 app.use('/info', infoRoutes);
 
-app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
